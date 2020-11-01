57d34a2aa7f70e35380878865fe64cfd7f040606
Nur <2020975359@isiswa.uitm.edu.my>
2020-11-01 13:51:52 +0000

    Commit for 3.5

--- 3.5.c
+++ 3.5.c
@@ -2,55 +2,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <errno.h>
 
-int main (void) {
-	void sigint_handler(int sig);
-	int fd[2],temp,prime,flag = 0;
-	pid_t pid = fork();
-	pipe(fd);
-	int a;
-	
-	if (signal(SIGINT, sigint_handler) == SIG_ERR) {
-		perror("signal");
-		exit(1);
-	}
+int main()
+{
+        int a=1;
+        int isprime=0;
+        int p[2];
+        pipe(p);
 
-	if(pid == 0) {
-		int a;
-		printf("Please enter an integer number:\n");
-		scanf("%d", &a);
-		write(fd[1],&a,sizeof(int));
-	}
+        int pid = fork();
+        if(pid < 0)
+                exit(1);
+        if(pid == 0)
+        {
+                close(p[1]);
+                read(p[0],&a,sizeof(int));
+                isprime = 1;
+                for(int i=2;i*i<=a;i++)
+                        if(a%i==0)
+                                isprime = 0;
+                if(isprime == 1)
+                        printf("%d is prime",a);
+                close(p[0]);
+        }
+        else
+        {
+                close(p[0]);
+                while(a!=0){
+                        printf("Please enter an integer:");
+                        scanf("%d",&a);
+                        write(p[1],&a,sizeof(int));
+                }
+                wait(0);
+                close(p[1]);
 
-	else if (pid >0) {
-		wait(NULL);
-		read(fd[0],&a,sizeof(int));
-		prime = a /2;
-	
-		for(int i=2; i<=prime; i++) {
-			if(prime % i == 0) {
-				flag =1;
-				break;
-			}
-		}
-		if (flag == 0) {
-			printf ("%d is a Prime Number\n");
-		}
-		else {
-			printf ("%d is not a Prime Number\n");
-		}
-		return 0;
-	}	
-	else {
-		printf("Error");
-		exit(1);
-	}
+        }
+
+        return 0;
 }
-
-	void sigint_handler(int sig)
-	{
-		printf("Terminate!\n");
-	}
